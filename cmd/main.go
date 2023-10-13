0dda094bf5d0227fb3484765ba8222e6f1fb43da
Giovanny472 <egmn472@yandex.com>
2023-10-13 08:36:03 +0300

    test data

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"image/png"
+	"log"
+
+	"github.com/kbinani/screenshot"
+	"golang.design/x/clipboard"
+)
 
 func main() {
-	fmt.Println("ss")
+
+	// screenshot
+	bounds := screenshot.GetDisplayBounds(0)
+	img, errScreen := screenshot.CaptureRect(bounds)
+	if errScreen != nil {
+		log.Fatalln("ошибка при screenshot: ", errScreen.Error())
+	}
+
+	// screen_image to bytes
+	reqBodyBytes := new(bytes.Buffer)
+	png.Encode(reqBodyBytes, img)
+
+	// init clipboard
+	errClip := clipboard.Init()
+	if errClip != nil {
+		log.Fatalln("ошибка при clipboard: ", errClip.Error())
+	}
+
+	// img to clipboard
+	clipboard.Write(clipboard.FmtImage, reqBodyBytes.Bytes())
+	clipboard.Read(clipboard.FmtImage)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/Giovanny472/grecordscreen
 
 go 1.21.3
+
+require (
+	github.com/gen2brain/shm v0.0.0-20230802011745-f2460f5984f7 // indirect
+	github.com/jezek/xgb v1.1.0 // indirect
+	github.com/kbinani/screenshot v0.0.0-20230812210009-b87d31814237 // indirect
+	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
+	golang.design/x/clipboard v0.7.0 // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/image v0.6.0 // indirect
+	golang.org/x/mobile v0.0.0-20230301163155-e0f57694e12c // indirect
+	golang.org/x/sys v0.11.0 // indirect
+)
