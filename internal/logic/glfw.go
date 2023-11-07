0b77d82a14953f4bf5336d2f23e2659abcbcc152
Giovanny472 <egmn472@yandex.com>
2023-11-07 22:14:01 +0300

    add draw

--- internal/logic/glfw.go
+++ internal/logic/glfw.go
@@ -1,8 +1,10 @@
 package logic
 
 import (
+	"fmt"
 	"log"
 	"runtime"
+	"strings"
 
 	"github.com/Giovanny472/grecordscreen/internal/constant"
 	"github.com/Giovanny472/grecordscreen/internal/message"
@@ -13,7 +15,7 @@ import (
 
 type glform struct {
 	screenGlfw *glfw.Window
-	opengl     uint32
+	openglProg uint32
 	mouse      model.Mouse
 	err        error
 }
@@ -23,7 +25,7 @@ var glf *glform
 func NewGlfw() model.GlFw {
 
 	if glf == nil {
-		glf = &glform{mouse: NewMouse(), opengl: 0}
+		glf = &glform{mouse: NewMouse(), openglProg: 0}
 	}
 	return glf
 }
@@ -76,6 +78,45 @@ func (g *glform) initOpenGL() {
 	}
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	log.Println("OpenGL version", version)
+
+	vertexShader, err := compileShader(constant.VertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		log.Fatal(message.VertexShaderCreate)
+	}
+
+	fragmentShader, err := compileShader(constant.FragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		log.Fatal(message.FragmentShaderCreate)
+	}
+
+	g.openglProg = gl.CreateProgram()
+	gl.AttachShader(g.openglProg, vertexShader)
+	gl.AttachShader(g.openglProg, fragmentShader)
+	gl.LinkProgram(g.openglProg)
+
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
 }
 
 func (g *glform) ShowGLFW() {
@@ -86,13 +127,47 @@ func (g *glform) programLoop() {
 
 	for !g.screenGlfw.ShouldClose() {
 
+		vao := g.makeVao()
+		g.draw(vao)
+
 		glfw.PollEvents()
 
 		// цвет background
 		gl.ClearColor(0.1, 0.1, 0.1, 0.05)
-		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		gl.UseProgram(g.openglProg)
+
 		g.screenGlfw.SetOpacity(0.3)
+
+		gl.BindVertexArray(vao)
+		triangle := []float32{0, 0.5, 0, -0.5, -0.5, 0, 0.5, -0.5, 0}
+		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle)/3))
 
 		g.screenGlfw.SwapBuffers()
 	}
+}
+
+func (g *glform) makeVao() uint32 {
+
+	points := []float32{0, 0.5, 0, -0.5, -0.5, 0, 0.5, -0.5, 0}
+
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+
+	return vao
+
+}
+
+func (g *glform) draw(vao uint32) {
+
 }
--- internal/message/message.go
+++ internal/message/message.go
@@ -1,7 +1,9 @@
 package message
 
 const (
-	GlfwInit         = "Невозможно инициализировать glfw"
-	GlfwCreateWindow = "Невозможно создать окно Screen"
-	OpenGLInit       = "Невозможно инициализировать opengl"
+	GlfwInit             = "Невозможно инициализировать glfw"
+	GlfwCreateWindow     = "Невозможно создать окно Screen"
+	OpenGLInit           = "Невозможно инициализировать opengl"
+	VertexShaderCreate   = "невозможно создать vertexShader"
+	FragmentShaderCreate = "невозможно создать fragmentShader"
 )
